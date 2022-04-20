d3e79d9c7c5e4b33086fe502c3b1b28f20725259
Lifewars <69715217+Lifewars@users.noreply.github.com>
2022-04-20 02:25:14 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+# Most of the code is explainable on it's own
 
 BLUE="\033[1;34m"
 NOCOLOR="\033[0m"
 
 echo
 
-echo -e "step 1: ${BLUE}Update Arch Linux Packages${NOCOLOR}"
+echo -e "${BLUE}Update Arch Linux Packages${NOCOLOR}"
 sudo pacman -Syyu
 
 echo 
 
-echo -e "step 2: ${BLUE}Update AUR Packages${NOCOLOR}"
+echo -e "${BLUE}Update AUR Packages${NOCOLOR}"
 paru -Sau
 
 echo 
 
-echo -e "step 3: ${BLUE}Update Flatpak Packages${NOCOLOR}"
+echo -e "${BLUE}Update Flatpak Packages${NOCOLOR}"
 flatpak update
 
 echo
